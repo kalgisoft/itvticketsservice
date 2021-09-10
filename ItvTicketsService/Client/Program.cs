5d311dafc185a4316c89c3ec6374f7d03614dc0c
Ritesh Ravlani <ritesh142@gmail.com>
2021-09-10 16:37:55 +0530

    Changes for Image Preview

--- ItvTicketsService/Client/Program.cs
+++ ItvTicketsService/Client/Program.cs
@@ -38,6 +38,7 @@ namespace ItvTicketsService.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddMudServices();
+            builder.Services.AddMudBlazorDialog();
 
             await builder.Build().RunAsync();
         }
